ebb7a402a46ab47aa511bd6eaf948c56dcf2538e
awswa <janghan@amazon.com>
2021-06-14 22:05:05 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# blog
+# Create a blog application running in Amplify (WIP)
+
+Started building a React frontend application with AWS serverless services such as Amplify, API Gateway, Lambda, and more...
+Backend is still work in progress...
+
+
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
     return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSmall"> React & Node </span>
-                <span className="headerTitleLarge"> Blog </span>
+                <span className="headerTitleSmall"> React & Node.js </span>
+                <span className="headerTitleLarge"> AWS Well-Architected Blog </span>
             </div>
             <img className="headerImg" src="https://retroianbucket.s3.ap-northeast-2.amazonaws.com/mike-enerio-7ryPpZK1qV8-unsplash.jpg" alt="">
             </img>
